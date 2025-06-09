d863e33aeaa13dacc8fa16159ef6ed502e26d2b3
acrysstein <cryslaine.stein@escola.pr.gov.br>
2025-06-09 12:11:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,47 @@
+criaCartao(
+    'Matemática',
+    'Qual a importância da matemática??',
+    'A matemática é crucial para o desenvolvimento de habilidades, o raciocínio lógico, a resolução de problemas e a compreensão do mundo ao nosso redor.'
+)
+
+criaCartao(
+    'Aritmética',
+    'Qual a diferença entre número par e número ímpar?',
+    'Um número par é sempre divisível por 2 sem deixar resto, enquanto um número ímpar não é divisível por 2.'
+)
+
+criaCartao(
+    'Álgebra',
+    'Qual é o valor de x na equação: 3x + 5 = 14?',
+    'x = 3'
+)
+
+criaCartao(
+    'Geometria',
+    'Quantos lados tem um hexágono?',
+    'Possui 6 lados.'
+)
+
+criaCartao(
+    'Esttatística e Probabilidade',
+    'O que é média aritmética?',
+    'É o valor obtido ao somar todos os valores de um conjunto de dados e, em seguida, dividir essa soma pelo número total de valores.'
+)
+
+criaCartao(
+    'Raciocínio Lógico',
+    'Um número mais o seu dobro é igual a 36. Qual é esse número?',
+    'Esse número é 12.'
+)
+
+criaCartao(
+    'Sistema de Numeração',
+    'O que é o sistema decimal?',
+    'É o sistema numerico que usamos no dia a dia, baseado em 10 algarismos (0 a 9).'
+)
+
+criaCartao(
+    'Medidas de Tempo',
+    'Quantos minutos tem uma hora?',
+    'Tem 60 minutos.'
+)
